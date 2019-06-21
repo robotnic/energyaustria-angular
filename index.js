16dfbf816feaaf207a2fbb6174d82e451aa5c0a0
bernhard <bernardo.interponti@gmail.com>
2019-06-21 02:44:11 +0200

    fixed syntax errors

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.all('/api/**', function(req, res) {
 app.use(express.static('dist/energy'));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/energy/index.html'));
+  res.sendFile('./dist/energy/index.html');
 });
 
 
